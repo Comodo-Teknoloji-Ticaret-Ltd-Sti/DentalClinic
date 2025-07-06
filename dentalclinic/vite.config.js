123ab8414375a0634f2f204a820783b3c06c5c58
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2025-07-06 23:53:26 +0300

    ..

--- dentalclinic/vite.config.js
+++ dentalclinic/vite.config.js
@@ -5,5 +5,4 @@ import react from '@vitejs/plugin-react'
 // "base" ayarını repo adına göre güncelleyin. Örnek: /REPO_ADI/
 export default defineConfig({
   plugins: [react()],
-  base: '/dentalclinic/'
 })
